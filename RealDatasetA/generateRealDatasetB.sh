16e7088783e2b985b0f58bb3f0afb94e0b09acf9
BenchWGBSanimal <96756011+Wentao-Gong@users.noreply.github.com>
2022-07-25 19:04:39 +0800

    Add files via upload

---
+++ RealDatasetA/generateRealDatasetB.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+soft=../soft
+realDataDir=../data
+
+species=human
+sampleList=(SRR6818517 SRR6373926 SRR6373932)
+for sample in "${sampleList[@]}"
+do
+	${soft}/seqtk-master/seqtk sample -s100 ${realDataDir}/${species}/${sample}/cleandata/${sample}_clean_1.fastq 1000000 > ${realDataDir}/${species}/seedReadLen/${sample}_clean_readLen150_1.fastq
+	${soft}/seqtk-master/seqtk sample -s100 ${realDataDir}/${species}/${sample}/cleandata/${sample}_clean_2.fastq 1000000 > ${realDataDir}/${species}/seedReadLen/${sample}_clean_readLen150_2.fastq
+done 
+
+species=cattle
+sampleList=(SRR7528450 SRR7528456 SRR7528458)
+for sample in "${sampleList[@]}"
+do
+	${soft}/seqtk-master/seqtk sample -s100 ${realDataDir}/${species}/${sample}/cleandata/${sample}_clean_1.fastq 1000000 > ${realDataDir}/${species}/seedReadLen/${sample}_clean_readLen150_1.fastq
+	${soft}/seqtk-master/seqtk sample -s100 ${realDataDir}/${species}/${sample}/cleandata/${sample}_clean_2.fastq 1000000 > ${realDataDir}/${species}/seedReadLen/${sample}_clean_readLen150_2.fastq
+done
+
+species=pig
+sampleList=(SRR7812176 SRR7812178 SRR7812179)
+for sample in "${sampleList[@]}"
+do
+	${soft}/seqtk-master/seqtk sample -s100 ${realDataDir}/${species}/${sample}/cleandata/${sample}_clean_1.fastq 1000000 > ${realDataDir}/${species}/seedReadLen/${sample}_clean_readLen150_1.fastq
+	${soft}/seqtk-master/seqtk sample -s100 ${realDataDir}/${species}/${sample}/cleandata/${sample}_clean_2.fastq 1000000 > ${realDataDir}/${species}/seedReadLen/${sample}_clean_readLen150_2.fastq
+done 
+
+
+
+ 
